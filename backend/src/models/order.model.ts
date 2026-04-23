dffb8d0edcf7e6b2b6cd468cda382127eb7f9cee
Ho Hoang Son <hoson2k5@gmail.com>
2026-04-23 20:40:48 +0700

    Order: Define model based on schema

---
+++ backend/src/models/order.model.ts
@@ -0,0 +1,22 @@
+import { Schema, model, Document, Types } from "mongoose";
+
+export interface IOrder extends Document {
+  branch_id: Types.ObjectId;
+  user_id: Types.ObjectId;
+  status: string;
+  total_amount: number;
+}
+
+const orderSchema = new Schema<IOrder>(
+  {
+    branch_id: { type: Schema.Types.ObjectId, ref: "Branch", required: true },
+    user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    status: { type: String, default: "pending" },
+    total_amount: { type: Number, required: true },
+  },
+  { timestamps: true },
+);
+
+const Order = model<IOrder>("Order", orderSchema);
+
+export default Order;
