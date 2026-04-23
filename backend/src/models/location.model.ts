822f27e177765ff531248d439d37f318eb3c31d3
Ho Hoang Son <hoson2k5@gmail.com>
2026-04-23 20:38:07 +0700

    Location: Define model based on schema

---
+++ backend/src/models/location.model.ts
@@ -0,0 +1,20 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface ILocation extends Document {
+  name: string;
+  status: string;
+  description: string;
+}
+
+const locationSchema = new Schema<ILocation>(
+  {
+    name: { type: String, required: true },
+    status: { type: String, default: "active" },
+    description: { type: String },
+  },
+  { timestamps: true },
+);
+
+const Location = model<ILocation>("Location", locationSchema);
+
+export default Location;
