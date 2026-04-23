2afaf5f8a07bda93721dfae91b5e1389c8cae4c5
Ho Hoang Son <hoson2k5@gmail.com>
2026-04-23 20:37:10 +0700

    Build asyncHandler to catch error

--- backend/src/helpers/asyncHandler.helper.ts
+++ backend/src/helpers/asyncHandler.helper.ts
@@ -1,14 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 
 const asyncHandler = (
-  fn: (req: Request, res: Response, next: NextFunction) => Promise<void>,
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<any>,
 ) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      fn(req, res, next);
-    } catch (error) {
-      next(error);
-    }
+    fn(req, res, next).catch(next);
   };
 };
 
